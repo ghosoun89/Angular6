9e456825db97f77063a0d8df9e16ca7806ecc4f8
ghosoun89 <goson.aldabea98@gmail.com>
2019-02-25 01:41:53 +0200

    creating ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+
+const routes:Routes =[
+  {path:'', redirectTo:'home' , pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'signin', component:SigninComponent},
+  {path:'signup', component:SignupComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     RouterModule.forRoot(routes) ,
   ],
   providers: [],
   bootstrap: [AppComponent]
